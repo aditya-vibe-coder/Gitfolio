5c2c011dbf6e25d5c37347c08ba275fe61f7d215
Aditya Yadav <aaaaaaaaaaditya890@gmail.com>
2026-06-10 19:13:35 +0530

    Delete gitfolio directory

--- gitfolio/src/components/ui/Skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Skeleton({ className = '' }) {
-  return (
-    <div className={`bg-[#161b22] animate-pulse border border-[#30363d] rounded ${className}`} />
-  );
-};
-
-
--- gitfolio/src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-
-const NotFound = () => (
-  <div className="flex items-center justify-center h-screen">
-    <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-  </div>
-);
-
-export default NotFound;
